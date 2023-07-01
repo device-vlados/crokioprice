a1238abda41dcbb7d2bc154041502a823715b56d
device-vlados <102617814+device-vlados@users.noreply.github.com>
2023-07-01 12:49:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -326,3 +326,17 @@ $(function () {
     });
     
 });
+$(document).ready(function() {
+    $('.call').on('click', function(e) {
+        e.preventDefault();
+        $('.calendly').toggleClass('calendly--open');
+    });
+    $('.calendly-close, .calendly-overlay').on('click', function() {
+        $('.calendly').removeClass('calendly--open');
+    });
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.calendly').length && !$(event.target).closest('.call').length) {
+            $('.calendly').removeClass('calendly--open');
+        }
+    });
+});
